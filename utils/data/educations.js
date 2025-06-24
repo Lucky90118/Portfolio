dbb8e8735a81a9dcc2c54aed7f17613a26135633
lucky376796 <146142344+Lucky90118@users.noreply.github.com>
2025-06-24 22:55:32 +0900

    add product

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
+    title: "Senior Software Engineer",
     duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    institution: "Hackerrank",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    title: "Junior Software Engineer",
+    duration: "2017 - 2019",
+    institution: "Hackerrank",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "Bachelor Degree",
+    duration: "2012 - 2016",
+    institution: "University of Parma",
   }
 ]
