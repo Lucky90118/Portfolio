2201dac08cd04cd35317bad331fef21aca397ed5
lucky376796 <146142344+Lucky90118@users.noreply.github.com>
2025-06-24 21:53:50 +0900

    add experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,26 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
+    title: 'Senior Software Engineer',
+    company: "ABC Technologies",
     duration: "(Jan 2022 - Present)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "Senior Software Engineer",
+    company: "Intern at DRDO CABS",
+    duration: "(Jun 2019 - Jan 2022)"
   },
   {
     id: 3,
-    title: "Self Employed",
+    title: "Junior Software Engineer",
     company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
-  }
+    duration: "(Jan 2014 - Jun 2019)"
+  },
+  {
+    id: 4,
+    title: "Full Stack Developer",
+    company: "Freelance",
+    duration: "(Jun 2012 - Jan 2014)"
+  },
 ]
