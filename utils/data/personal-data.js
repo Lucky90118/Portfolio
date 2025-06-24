147aa5d6cd501eedf79796e5e1b9f5fb435c406f
lucky376796 <146142344+Lucky90118@users.noreply.github.com>
2025-06-24 11:52:23 +0900

    update portfolio

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,8 +2,8 @@ export const personalData = {
   name: "ABU SAID",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'abusaid7388@gmail.com',
+  description: "My name is Abu Said. I am a professional and passionate programmer by day. I am a fast learner and have a self-learning attitude. I love learning and exploring new technologies and am passionate about problem solving. I love almost all stacks of web application development and I love making the web more open to the world. My core technology is based on JavaScript and I like to do most of my work using JavaScript. I also have a lot of experience in game development including unity and Blockchain development which is becoming a global trend now. Based on my skills and experience, I am open to applying for any kind of job that suits my interests.",
+  email: 'fucabruno118@gmail.com',
   phone: '+8801608797655',
   address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
   github: 'https://github.com/said7388',
@@ -12,6 +12,6 @@ export const personalData = {
   twitter: 'https://twitter.com/said7388',
   stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
   leetcode: "https://leetcode.com/said3812/",
-  devUsername: "said7388",
+  devUsername: "BrunoCat118",
   resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
 }
