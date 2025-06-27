276e9e2c7560c3ec3b10b8e2ac62252c9ffdb8b3
lucky376796 <146142344+Lucky90118@users.noreply.github.com>
2025-06-27 11:48:08 +0900

    project

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
-module.exports = async () => {
-  const netlifyPlugin = await import('@netlify/plugin-nextjs');
-  return netlifyPlugin.default(nextConfig);
-};
+module.exports = nextConfig;
